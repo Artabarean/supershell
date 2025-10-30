3781725e38bba43da1f277d3ade36f7b27e036a2
Alex Robert Tabarean <atabarea@c2r15s6.42madrid.com>
2025-10-30 19:20:26 +0100

    execute_ done

--- execution/srcs/execute_.c
+++ execution/srcs/execute_.c
@@ -6,7 +6,7 @@
 /*   By: atabarea <atabarea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 17:48:09 by atabarea          #+#    #+#             */
-/*   Updated: 2025/10/30 18:13:40 by atabarea         ###   ########.fr       */
+/*   Updated: 2025/10/30 19:19:34 by atabarea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,18 @@
 
 void	execute_(char **full_cmd, char *full_path, t_prompt prompt)
 {
-	pid_t	pid;
+	int	fd[2];
+
+	prompt.pid = fork();
+	if (prompt.pid == -1)
+		return (error);
+	if (prompt.pid == 0)
+	{
+		dup2(fd[0], 0);
+		dup2(fd[1], 1);
+		if (!full_path)
+			error();
+		if (execve(full_path, full_cmd, prompt.enviroment->envp) == -1)
+			error();
+	}
 }
