722a8f68f1bccec04bef709c1ff381edd84f1677
Mercurie67 <meru334@gmail.com>
2025-11-17 16:45:58 +0100

    Add files via upload

--- minishell/enviroment.c
+++ minishell/enviroment.c
@@ -3,38 +3,61 @@
 /*                                                        :::      ::::::::   */
 /*   enviroment.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: medel-ca <medel-ca@student.42.fr>          +#+  +:+       +#+        */
+/*   By: medel-ca <medel-ca@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 20:06:53 by medel-ca          #+#    #+#             */
-/*   Updated: 2025/10/30 20:06:53 by medel-ca         ###   ########.fr       */
+/*   Updated: 2025/11/04 09:32:25 by medel-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
 
-void	parse_env(t_env *e, char **env)
+void	init_env(t_prompt *prompt, char **env)
+{
+	int		i;
+	t_env	*head;
+	t_env	*current;
+
+	if (!prompt || !env)
+		return ;
+	head = new_env();
+	if (!head)
+		return ;
+	prompt->enviroment = head;
+	current = head;
+	i = 0;
+	while (env[i])
+	{
+		fill_env(env[i], current);
+		if (env[i + 1])
+		{
+			current->next = new_env();
+			current = current->next;
+		}
+		i++;
+	}
+}
+
+void	fill_env(char *env, t_env *e)
 {
 	int	i;
 
 	i = 0;
-	while (env[i])
-	{
-		if (!ft_strncmp(env[i], "PATH=", 5))
-			e->path = ft_strdup(env[i] + 5);
-		else if (!ft_strncmp(env[i], "HOME=", 5))
-			e->home = ft_strdup(env[i] + 5);
-		else if (!ft_strncmp(env[i], "PWD=", 4))
-			e->pwd = ft_strdup(env[i] + 4);
-		else if (!ft_strncmp(env[i], "OLDPWD=", 7))
-			e->oldpwd = ft_strdup(env[i] + 7);
-		else if (!ft_strncmp(env[i], "USER=", 5))
-			e->user = ft_strdup(env[i] + 5);
-		else if (!ft_strncmp(env[i], "SHELL=", 6))
-			e->shell = ft_strdup(env[i] + 6);
-		else if (!ft_strncmp(env[i], "SHLVL=", 6))
-			e->shlvl = ft_atoi(env[i] + 6);
-		else if (!ft_strncmp(env[i], "_=", 2))
-			e->cmdpath = ft_strdup(env[i] + 2);
+	while (env[i] && env[i] != '=')
 		i++;
-	}
+	e->keyword = ft_substr(env, 0, i);
+	e->value = ft_substr(env, i + 1, (ft_strlen(env) - (i + 1)));
+}
+
+t_env	*new_env(void)
+{
+	t_env	*current;
+
+	current = ft_calloc(1, sizeof(t_env));
+	if (!current)
+		return (NULL);
+	current->keyword = NULL;
+	current->value = NULL;
+	current->next = NULL;
+	return (current);
 }
