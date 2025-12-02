b6fd2d459669f7931e6550b8976a70ddd9efe414
Alex Robert Tabarean <atabarea@c2r15s4.42madrid.com>
2025-12-02 13:11:03 +0100

    echo done

--- minishell/echo.c
+++ minishell/echo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*   By: atabarea <atabarea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/13 09:44:43 by codespace         #+#    #+#             */
-/*   Updated: 2025/11/28 19:34:29 by codespace        ###   ########.fr       */
+/*   Updated: 2025/12/02 13:07:00 by atabarea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,11 @@ void	echo(char **full_cmd)
 	i = 1;
 	while (full_cmd[i])
 	{
-		if (full_cmd[0][0] == '-')
-			return ;
 		if (ft_strncmp(full_cmd[i], "-n", 2) == 0)
 		{
-			i++;
-			if (full_cmd[i][0] == '-' || full_cmd[i] == NULL)
+			while (full_cmd[i] != NULL && ft_strncmp(full_cmd[i], "-n", 2) == 0)
+				i++;
+			if (full_cmd[i] == NULL)
 				return ;
 			else
 				(printf("%s", full_cmd[i]));
