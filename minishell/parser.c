f5bea79abf10f38c67a16a682f910907f9e9f92b
Artabarean <artabarean@gmail.com>
2026-01-15 12:43:00 +0000

    unset ya casi esta

--- minishell/parser.c
+++ minishell/parser.c
@@ -58,7 +58,7 @@ bool	parser(t_prompt *prompt, t_cmd *curr)
 		else
 			handle_word(prompt, curr, &i);
 	}
-	if (!curr->full_cmd || (!curr->full_cmd[0] && !curr->redir))
+	if (!curr->full_cmd || !curr->full_cmd[0])
 		return (syntax_error("newline"), false);
 	curr->next = NULL;
 	return (true);
