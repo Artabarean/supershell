335da6a5484ac504826cbc0e0d50760f9f4f2ddd
Artabarean <atabarea@c1r15s5.42madrid.com>
2026-02-09 18:13:07 +0100

    empty string fix

--- minishell/src/utils/cmd_isdir.c
+++ minishell/src/utils/cmd_isdir.c
@@ -6,7 +6,7 @@
 /*   By: atabarea <atabarea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/04 11:06:21 by atabarea          #+#    #+#             */
-/*   Updated: 2026/02/05 14:57:59 by atabarea         ###   ########.fr       */
+/*   Updated: 2026/02/09 18:10:52 by atabarea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,8 @@ int	cmd_isdir(t_cmd *cmd, char *full_cmd)
 {
 	if (full_cmd)
 	{
+		if (!cmd->full_cmd[0][0])
+			return (1);
 		if (ft_strchr(full_cmd, '/'))
 		{
 			if (full_cmd[0] == '.')
